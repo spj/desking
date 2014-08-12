553d52a0d7354e82668a48ad6c73df9f4dc145e8
spj <shipj@hotmail.com>
2014-08-11 21:45:14 -0400

    --1st checkin

---
+++ desking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(desking.Startup))]
+namespace desking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
